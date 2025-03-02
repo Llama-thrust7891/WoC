a4acee204eac868b9187cab4d85fc69d0cbf5cd1
Llama-thrust7891 <blackknight7891@gmail.com>
2025-03-02 21:45:27 +1100

    bug-fix

--- 01_WoC_Index.lua
+++ 01_WoC_Index.lua
@@ -1,8 +1,9 @@
 ------------Coldwar 78 index----------------
 --------------------------------------------
 ---
-local lfs = require("lfs")
-local savedGamesPath = lfs.writedir() -- This gets "C:/Users/YourUsername/Saved Games/DCS/" dynamically
+--local lfs = require("lfs")
+--local savedGamesPath = lfs.writedir() -- This gets "C:/Users/YourUsername/Saved Games/DCS/" dynamically
+dofile(lfs.writedir() .. "Missions/WoC-Sinai/Moose.lua")
 dofile(lfs.writedir() .. "Missions/WoC-Sinai/04_WoC_Groups.lua")
 dofile(lfs.writedir() .. "Missions/WoC-Sinai/03_WoC_Main.lua")
 
